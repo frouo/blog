7776a7b67d846662f059f900bc14a791fd62312f
François Rouault <francois.rouault@cocoricostudio.com>
2021-01-28 17:02:36 +0100

    Revert "Add Google Analytics"
    This reverts commit ce6e2f5b5889bf1922e416f0a5e5279a88a04c38.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,6 @@
 import { AppProps } from "next/app";
-import { useRouter } from "next/router";
 import "../styles/index.css";
-import * as gtag from "../lib/gtag";
-import { useEffect } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { GA_TRACKING_ID } from "../lib/gtag";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          {process.env.NODE_ENV !== "development" && (
-            <>
-              <GoogleAnalytics />
-            </>
-          )}
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
@@ -20,30 +13,6 @@ export default class MyDocument extends Document {
       </Html>
     );
   }
-}
-
-function GoogleAnalytics() {
-  return (
-    <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-      />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
-    </>
-  );
 }
 
 function SimpleAnalytics() {
