0eeed29bdf760360e773e4b64a13815d46562407
François Rouault <francois.rouault@cocoricostudio.com>
2021-04-21 19:07:44 +0200

    Remove Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,9 @@
 import { AppProps } from "next/app";
 import "../styles/index.css";
 import { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as gtm from "../lib/gtm";
 import splitbee from "@splitbee/web";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      gtm.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   useEffect((): void => {
     splitbee.init({
       scriptUrl: "/bee.js",
